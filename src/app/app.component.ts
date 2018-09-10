54c6d547b7a2c810a05a19e778a2a99dd493a64d
Agostino Cecere <agostino.cecere@outlook.it>
2018-09-10 22:48:21 +0200

    Added project - first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-ecommerce',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+
+  products : any[] = [
+    {isbn : 'JK45JOL55', name :  'Il Conte di Montecristo', price : 19.99, qty : 5},
+    {isbn : 'PH78KLD76', name :  'Dieci Piccoli Indiani', price : 9.99, qty : 7},
+    {isbn : 'BD23HUL82', name :  'Notti Bianche', price : 14.99, qty : 3}
+  ]
+
+  constructor() {  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './inc/navbar/navbar.component';
+import { BookComponent } from './book/book.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    BookComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [NavbarComponent, AppComponent]
 })
 export class AppModule { }
